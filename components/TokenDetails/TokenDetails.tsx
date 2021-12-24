3c3c516bd722fb3f14f32a4d98a6360297209a92
Nikola Gigic <nik.gigic@gmail.com>
2021-12-25 00:13:19 +0100

    fix nft details toolbar

--- components/TokenDetails/TokenDetails.tsx
+++ components/TokenDetails/TokenDetails.tsx
@@ -4,11 +4,37 @@ import Box from "@mui/material/Box";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import Divider from "@mui/material/Divider";
-import { TextField, Button, Typography } from "@mui/material";
+import { Grid, Button, Typography } from "@mui/material";
 
 import { buyNft } from "../../utils/apiHelpers";
 
 import { styled } from "@mui/material";
+import { borderColor } from "@mui/system";
+
+const StyledTypography = styled(Typography)(({ theme }) => ({
+  width: 217,
+  color: 'white',
+  fontSize: 'inherit',
+  paddingBottom: 4,
+}))
+
+const StyledDescription = styled(Typography)(({ theme }) => ({
+  width: 260,
+  minHeight: 368,
+  fontWeight: 400,
+  color: 'white',
+  fontSize: 'inherit',
+  padding: '16.5px 14px 16.5px 14px',
+  borderRadius: 4,
+  border: 'solid 1px rgba(255, 255, 255, .7)'
+}))
+
+const StyledPriceValue = styled(Typography)(({ theme }) => ({
+  color: 'rgba(255, 255, 255, .7)',
+  fontSize: 'inherit',
+  paddingBottom: 4,
+  float: 'right'
+})) 
 
 interface StudioToolbarProps {
   NFT: {
@@ -23,7 +49,7 @@ const BasicList: FC<StudioToolbarProps> = ({ NFT }) => {
   return (
     <Box
       sx={{
-        maxWidth: 360,
+        width: '100%',
         height: "100%",
         bgcolor: "background.paper",
         display: "flex",
@@ -32,13 +58,29 @@ const BasicList: FC<StudioToolbarProps> = ({ NFT }) => {
     >
       <List>
         <ListItem>
-          <Typography color={"white"}>{NFT.name}</Typography>
+          <Box>
+            <Typography fontSize={'0.75rem'} color={'white'} sx={{opacity: 0.7}}>Name</Typography>
+            <StyledTypography sx={{borderBottom: 'solid 1px rgba(255, 255, 255, .7)'}}>{NFT.name}</StyledTypography>
+          </Box>
         </ListItem>
         <ListItem>
-          <Typography color={"white"}>{NFT.description}</Typography>
+          <Box>
+            <Typography fontSize={'0.75rem'} color={'rgba(255, 255, 255, .7)'} sx={{position: 'absolute', marginLeft: 1, marginTop: -1.55, padding: 0.5, backgroundColor: '#121212'}} component={'label'}>Description</Typography>
+            <StyledDescription>{NFT.description}</StyledDescription>
+          </Box>
         </ListItem>
         <ListItem>
-          <Typography color={"white"}>{NFT.price}</Typography>
+        <Box sx={{borderBottom: 'solid 1px rgba(255, 255, 255, .7)'}}>
+          <Typography fontSize={'0.75rem'} color={'white'} sx={{opacity: 0.7}} component={'label'}>Price</Typography>
+          <Grid container>
+            <Grid item xs={10}>
+              <StyledTypography>{NFT.price}</StyledTypography>
+            </Grid>
+            <Grid item xs={2}>
+              <StyledPriceValue>ETH</StyledPriceValue>
+            </Grid>
+          </Grid>
+          </Box>
         </ListItem>
         <Divider />
         <ListItem>
